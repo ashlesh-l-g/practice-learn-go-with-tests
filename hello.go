76efe246eb6acf6062bb28991ef687586228bd0b
Ashleshlg <ash4224.dev@gmail.com>
2026-08-28 19:56:48 +0530

    Initial commit

---
+++ hello.go
@@ -0,0 +1,30 @@
+package main
+
+import "fmt"
+
+const french = "French"
+const spanish = "Spanish"
+const englishHelloPrefix = "Hello, "
+const spanishHelloPrefix = "Hola, "
+const frenchHelloPrefix = "Bonjour, "
+
+func Hello(name string,language string) string {
+	if name == ""{
+		name = "world"
+	}
+
+	prefix := englishHelloPrefix
+
+	switch language {
+	case spanish:
+		prefix = spanishHelloPrefix
+	case french:
+		prefix = frenchHelloPrefix
+	}
+
+	return prefix + name
+}
+
+func main(){
+	fmt.Println(Hello("English","world"))
+}
